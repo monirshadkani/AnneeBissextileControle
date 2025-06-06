8638a54c8e426942c8773c0a9ead0473f0e0daaf
monirshadkani <152501836+monirshadkani@users.noreply.github.com>
2025-06-06 14:47:57 +0200

    REFACTO Merged the conditions

--- AnneeBissextile/GregorianCalendar.cs
+++ AnneeBissextile/GregorianCalendar.cs
@@ -5,14 +5,11 @@ namespace AnneeBissextile
     {
         public static bool IsLeapYear(int year)
         {
-            if ((year % 400 == 0) || (year % 100 != 0 && year % 400 == 0) )
+            if ((year % 400 == 0) || (year % 100 != 0 && year % 400 == 0)  || (year % 4 == 0 && year % 100 != 0))
             {
                 return true;
             }
-            if (year % 4 == 0 && year % 100 != 0)
-            {
-                return true;
-            }
+
 
             return false;
         }

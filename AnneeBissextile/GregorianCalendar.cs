8aeaa706028487bff382e4f7c8518400658d0309
monirshadkani <152501836+monirshadkani@users.noreply.github.com>
2025-06-06 14:47:23 +0200

    [GREEN]

--- AnneeBissextile/GregorianCalendar.cs
+++ AnneeBissextile/GregorianCalendar.cs
@@ -9,6 +9,10 @@ namespace AnneeBissextile
             {
                 return true;
             }
+            if (year % 4 == 0 && year % 100 != 0)
+            {
+                return true;
+            }
 
             return false;
         }

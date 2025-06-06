ed7293513c2ca60042dc7e336c0eacf77043e27e
monirshadkani <152501836+monirshadkani@users.noreply.github.com>
2025-06-06 14:36:43 +0200

    [RED] divisible by 100 and not by 400 returns true, should return false

--- AnneeBissextile.Test/UnitTest1.cs
+++ AnneeBissextile.Test/UnitTest1.cs
@@ -2,15 +2,40 @@ namespace AnneeBissextile.Test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void IsDivisibleBy400()
+
+
+
+
+        [Theory]
+        [InlineData(400)]
+        [InlineData(800)]
+        [InlineData(1200)]
+        [InlineData(1600)]
+
+        public void IsDivisibleBy400(ushort year)
         {
             //Given a year that is divisible by 400
             //When I check if it is a leap year
-            var isLeap = GregorianCalendar.IsLeapYear(400);
+            var isLeap = GregorianCalendar.IsLeapYear(year);
             //Then it should return true
             Assert.True(isLeap);
-
         }
+
+        [Theory]
+        [InlineData(100)]
+        [InlineData(200)]
+        [InlineData(500)]
+
+
+        public void IsDivisibleBy100NotDivisibleBy400(ushort year)
+        {
+            //Given a year that is divisible by 100 but not by 400
+            //When I check if it is a leap year
+            var isLeap = GregorianCalendar.IsLeapYear(year);
+            //Then it should return false
+            Assert.False(isLeap);
+        }
+
+
     }
 }

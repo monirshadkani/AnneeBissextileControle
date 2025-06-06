cb4f6137b39cf3454b959d43be9d76d00ac4c420
monirshadkani <152501836+monirshadkani@users.noreply.github.com>
2025-06-06 14:29:05 +0200

    [RED] is divisible by 400 returns false instead of true

--- AnneeBissextile.Test/UnitTest1.cs
+++ AnneeBissextile.Test/UnitTest1.cs
@@ -3,8 +3,13 @@ namespace AnneeBissextile.Test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void IsDivisibleBy400()
         {
+            //Given a year that is divisible by 400
+            //When I check if it is a leap year
+            var isLeap = GregorianCalendar.IsLeapYear(400);
+            //Then it should return true
+            Assert.True(isLeap);
 
         }
     }
---
+++ AnneeBissextile/GregorianCalendar.cs
@@ -0,0 +1,11 @@
+﻿
+namespace AnneeBissextile
+{
+    public class GregorianCalendar
+    {
+        public static bool IsLeapYear(int year)
+        {
+            return false;
+        }
+    }
+}
